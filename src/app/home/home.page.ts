de1296a31ac0c803c38ec4d8da2ace2e899a0675
ssampathirao <ssampathirao@gmail.com>
2019-02-01 23:16:48 -0500

    logo added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,25 +10,3 @@ export class HomePage{
 	constructor() {}
 }
 
-@Component({
-  selector: 'slides-example',
-  template: `
-    <ion-slides pager="true" [options]="slideOpts">
-      <ion-slide>
-        <h1>Slide 1</h1>
-      </ion-slide>
-      <ion-slide>
-        <h1>Slide 2</h1>
-      </ion-slide>
-      <ion-slide>
-        <h1>Slide 3</h1>
-      </ion-slide>
-    </ion-slides>
-  `
-})
-export class SlideExample {
-  slideOpts = {
-    effect: 'flip'
-  };
-  constructor() {}
-}
