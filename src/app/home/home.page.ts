60718a92705e0d7f00e1cbee676028afeb4cc7ac
ssampathirao <ssampathirao@gmail.com>
2019-02-06 23:32:19 -0500

    updated slide autoplay

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import { Slides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,80 @@ import { Component} from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 
-export class HomePage{
-	constructor() {}
+export class HomePage
+{
+
+@ViewChild('slideWithNav') slideWithNav: Slides;
+
+
+sliderHome: any;
+
+slideOptsHome = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay:true
+  };
+
+constructor() {
+
+    this.sliderHome =
+      {
+        isBeginningSlide: true,
+        isEndSlide: false,
+        slidesItems: [
+          {
+            id: 1,
+            image: '../../assets/Sankranthi4.jpg'
+          },
+          {
+            id: 2,
+            image: '../../assets/CATSDasara2017.jpeg'
+          },
+          {
+            id: 3,
+            image: '../../assets/CATS_DCcentral.jpg'
+          }
+        ]
+      };
+	
 }
+
+  //Move to Next slide
+  slideNext(object, slideView) {
+    slideView.slideNext(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });
+  }
+ 
+  //Move to previous slide
+  slidePrev(object, slideView) {
+    slideView.slidePrev(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });;
+  }
+ 
+  //Method called when slide is changed by drag or navigation
+  SlideDidChange(object, slideView) {
+    this.checkIfNavDisabled(object, slideView);
+  }
+ 
+  //Call methods to check if slide is first or last to enable disbale navigation  
+  checkIfNavDisabled(object, slideView) {
+    this.checkisBeginning(object, slideView);
+    this.checkisEnd(object, slideView);
+  }
+ 
+  checkisBeginning(object, slideView) {
+    slideView.isBeginning().then((istrue) => {
+      object.isBeginningSlide = istrue;
+    });
+  }
+  checkisEnd(object, slideView) {
+    slideView.isEnd().then((istrue) => {
+      object.isEndSlide = istrue;
+    });
+  }	
+  
+}
+
 
